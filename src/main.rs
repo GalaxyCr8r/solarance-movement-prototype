a43e469c1adaf6309305565fb34279e0a33181e8
Karl Nyborg <karl.nyborg@blackcape.io>
2026-02-10 22:19:34 -0500

    egui now working, printing text in middle screen

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod module_bindings;
+use macroquad::math::Vec2;
 use macroquad::prelude::{collections::storage, *};
 use macroquad::window::Conf;
 use module_bindings::*;
@@ -6,6 +7,17 @@ use std::env;
 use std::io::{self, Read};
 
 use spacetimedb_sdk::{DbContext, Table, Timestamp};
+
+pub struct GameState<'a> {
+    // Game-Wide States
+    pub done: bool,
+    pub ctx: &'a DbConnection,
+
+    // Display States
+    pub camera: Camera2D,
+    pub bg_camera: Camera2D,
+    // GUI States
+}
 
 /// Configures the game window properties including title, dimensions, and resizability
 fn window_conf() -> Conf {
@@ -24,11 +36,53 @@ async fn main() -> Result<(), macroquad::Error> {
     set_pc_assets_folder("assets");
     clear_background(BLACK);
     next_frame().await;
-    connect();
+
+    let mut game_state = GameState {
+        done: false,
+        ctx: &connect(),
+        camera: Camera2D::from_display_rect(Rect {
+            x: 0.0,
+            y: 0.0,
+            w: screen_width(),
+            h: screen_height(),
+        }),
+        bg_camera: Camera2D::from_display_rect(Rect {
+            x: 0.0,
+            y: 0.0,
+            w: screen_width(),
+            h: screen_height(),
+        }),
+    };
+    game_state.camera.zoom.y *= -1.0;
+    game_state.bg_camera.zoom.y *= -1.0;
+
+    loop {
+        clear_background(BLACK);
+
+        game_state.camera.target = Vec2::ZERO; //get_player_transform_vec2(&ctx, Vec2::ZERO);
+        set_camera(&game_state.camera);
+
+        draw_text("Henlo!", 0.0, 0.0, 42.0, WHITE);
+
+        egui_macroquad::ui(|egui_ctx| {
+            egui::SidePanel::left("left_panel").show(egui_ctx, |ui| {
+                ui.heading("Solarance:Beginnings");
+            });
+        });
+
+        egui_macroquad::draw();
+        next_frame().await;
+
+        if game_state.done {
+            let _ = game_state.ctx.disconnect();
+            break;
+        }
+    }
+
     Ok(())
 }
 
-fn connect() {
+fn connect() -> DbConnection {
     // The URI of the SpacetimeDB instance hosting our chat module.
     let host: String = env::var("SPACETIMEDB_HOST").unwrap_or("http://localhost:3000".to_string());
 
@@ -51,6 +105,8 @@ fn connect() {
         .expect("Failed to connect");
 
     conn.run_threaded();
+
+    conn
 }
 
 // fn main() {
